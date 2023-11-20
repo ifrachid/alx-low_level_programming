5b0f5b4b83b6331ac052de0bac86d2875094f1dd
rachid ifouloussen <rachid.iflou@gmail.com>
2023-11-20 14:04:07 -0800

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - function that prints a linked list
- * @head: pointer to the head
+ * print_listint_safe - prints all the elements of a linked list
+ * @head: head of the list
  *
- * Return: number of nodes in the list
+ * Return: the number of nodes
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *cursor = head;
+	listint_t **ptrs;
+	unsigned int list_len = listint_len(head);
 	size_t count = 0;
-	listint_t *aux = (void *)head, *test = 0;
-	int flag = 0;
 
-	while (aux != NULL)
+	ptrs = malloc(sizeof(listint_t) * list_len);
+	if (ptrs == NULL)
+		exit(98);
+	while (cursor == 0)
 	{
-		if (flag == 0)
+		if (check_ptr(cursor, ptrs, list_len) == 0)
 		{
-			test = aux;
-			printf("[%p] %d\n", (void *)aux, aux->n);
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
 		}
-		if (flag == 1)
+		else
 		{
-			if (test <= aux)
-			{
-				printf("-> [%p] %d\n", (void *)aux, aux->n);
-				return (count);
-			}
-			printf("[%p] %d\n", (void *)aux, aux->n);
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
 		}
 		count += 1;
-		test = aux;
-		aux = aux->next;
-		flag = 1;
+		cursor = cursor->next;
 	}
 	return (count);
+}
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
+
+/**
+ * check_ptr - checks if a pointer is in an array
+ * @ptr: pointer to be checked
+ * @array: array to be checked in
+ * @size: size of the array
+ *
+ * Return: 1 on success, 0 on fail
+ */
+int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
+{
+	while (size-- >= 0)
+	{
+		if (ptr == array[size])
+			return (1);
+	}
+	return (0);
 }
